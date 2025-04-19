23d3a5b49f5f804005bda9981fc72d1e6552522f
rikster-r <seitdanial43@gmail.com>
2025-04-19 20:53:04 +0500

    Fix sidebar burger color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { User } from '@supabase/supabase-js';
 import Sidebar from './Sidebar';
+import { useRouter } from 'next/router';
 
 type Props = {
   user: User;
@@ -16,13 +17,20 @@ const Layout = ({
   mainClassName,
   titleClassName,
 }: Props) => {
+  const router = useRouter();
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       <main
         className={`flex-1 w-full lg:w-[calc(100%-320px)] lg:ml-80 flex flex-col ${mainClassName}`}
       >
         <div className="flex items-center">
-          <Sidebar user={user} buttonClassName="text-black" />
+          <Sidebar
+            user={user}
+            buttonClassName={
+              router.asPath === '/' ? 'text-white' : 'text-black'
+            }
+          />
           <h1
             className={`w-full mx-auto text-xl font-bold py-4 lg:py-6 lg:pl-16 text-left ${titleClassName}`}
           >
