4d6966f1ff1eab056486e810409da79c6b1e5df0
rikster-r <seitdanial43@gmail.com>
2025-04-25 20:04:06 +0500

    Add token refreshing

--- src/pages/api/avito/accessToken/index.ts
+++ src/pages/api/avito/accessToken/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import createClient from '@/lib/supabase/api';
-import { encrypt } from '@/lib/encrypt';
+import { decrypt, encrypt } from '@/lib/encrypt';
+import { addSeconds, isAfter, parseISO } from 'date-fns';
 
 export default async function handler(
   req: NextApiRequest,
@@ -70,13 +71,64 @@ export default async function handler(
         .json({ error: 'Войдите в аккаунт перед запросом' });
     }
 
-    const { data: tokenData, error } = await supabase
+    let { data: tokenData, error: tokenError } = await supabase
       .from('avito_access_tokens')
       .select('*')
       .eq('user_id', user.id)
       .single();
 
-    if (error) return res.status(500).json({ error: error.message });
+    if (tokenError) return res.status(500).json({ error: tokenError.message });
+
+    const createdAt = parseISO(tokenData.created_at);
+    const expiresAt = addSeconds(createdAt, tokenData.expires_in);
+    const isExpired = isAfter(new Date(), expiresAt);
+
+    if (!isExpired) {
+      return res.status(200).json(tokenData);
+    }
+
+    // refresh token if expired
+    const params = new URLSearchParams();
+    params.append('grant_type', 'refresh_token');
+    params.append(
+      'client_id',
+      process.env.NEXT_PUBLIC_AVITO_CLIENT_ID as string
+    );
+    params.append('client_secret', process.env.AVITO_CLIENT_SECRET as string);
+    params.append('refresh_token', decrypt(tokenData.refresh_token));
+
+    const refreshRes = await fetch('https://api.avito.ru/token/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: params.toString(),
+    });
+
+    const refreshData = await refreshRes.json();
+
+    if (!refreshRes.ok) {
+      return res.status(500).json({ error: refreshData });
+    }
+
+    // update data after token refresh
+    ({ data: tokenData, error: tokenError } = await supabase
+      .from('avito_access_tokens')
+      .update([
+        {
+          access_token: encrypt(refreshData.access_token),
+          expires_in: refreshData.expires_in,
+          scope: refreshData.scope,
+          refresh_token: encrypt(refreshData.refresh_token),
+          token_type: refreshData.token_type,
+        },
+      ])
+      .eq('user_id', user.id));
+
+    if (tokenError) {
+      return res.status(500).json({ error: tokenError.message });
+    }
+
     return res.status(200).json(tokenData);
   }
 }
